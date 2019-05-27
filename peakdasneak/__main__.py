01edd800e055b98869f85937b1b6812e1afcbe95
lacanlale <gizmo1398@gmail.com>
2019-05-27 16:36:38 -0700

    WOW just finished getting live sentiment analysis on comments and displaying it to screen. Also added a navigation menu for the project

--- peakdasneak/__main__.py
+++ peakdasneak/__main__.py
@@ -3,35 +3,74 @@ from .formatter import Formatter
 from threading import Timer
 from .reddit_scraper import Reddit_Scraper
 from .secret import *
+from webbrowser import open as wbopen
 import sys
 import os
 
+prmpt = Formatter()
+prmpt.cfg('w', st='b')
 
 def clr():
         os.system('cls' if os.name=='nt' else 'clear')
 
 
-def main():
-    prmpt = Formatter()
-    prmpt.cfg('w', st='b')
-    prmpt.out('{:^170}'.format("Welcome to peakdasneak-cli, a tool for assistance with sneaker reselling."))
-    reddit = Reddit_Scraper(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, version=0.1)
+def disp_goatx():
+    prmpt.out("Presented is info on StockX for deadstock shoes and the lowest prices on GOAT for the same popular shoes.")
+    scraper = Scraper()
+    shoe_info = scraper.scrape_info()
+    scraper_thread = Timer(500.0, scraper.scrape_info)
+    scraper_thread.start()
+    prmpt.out('Use s to search for a shoes pricing')
+    while True:
+        action = input(">>> ")
+        if action == 's':
+            scraper_thread.cancel()
+            prmpt.out("Input shoe name")
+            scraper.search(input(">>> "))
 
+
+def disp_reddit():
+    reddit = Reddit_Scraper(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, version=0.1)
     prmpt.out("Displaying latest release news collected by Reddit")
     prmpt.out("Date format is: MM/DD/YYYY")
-    reddit.scrape(subreddit='sneakers', flair='release', limit=100, sort_by='top', posts_from='week')
-    # prmpt.out('{:^170}'.format("Presented is info on StockX for deadstock shoes and the lowest prices on GOAT for the same popular shoes."))
-    # scraper = Scraper()
-    # shoe_info = scraper.scrape_info()
-    # scraper_thread = Timer(500.0, scraper.scrape_info)
-    # scraper_thread.start()
-    # prmpt.out('Use s to search for a shoes pricing')
-    # while True:
-    #     action = input(">>> ")
-    #     if action == 's':
-    #         scraper_thread.cancel()
-    #         prmpt.out("Input shoe name")
-    #         scraper.search(input(">>> "))
+    urls = reddit.scrape(subreddit='sneakers', flair='release', limit=500, sort_by='top', posts_from='month')
+    prmpt.out("Enter the number of the post to open || Enter e to exit to main menu")
+    inp = input(">>> ")
+    
+    while not inp in urls and not inp == 'e':
+        inp = input(">>> ")
+    
+    if inp == 'e':
+        clr()
+        disp_main()
+        return
+
+    wbopen(urls[inp])
+    disp_reddit()
+
+
+def disp_main():
+    prmpt.out('{:^170}'.format("Welcome to peakdasneak-cli, a tool for assistance with sneaker reselling."))
+    prmpt.out('{:^170}'.format("Select a display option"))
+    prmpt.out('1.) Display Reddit data on new releases')
+    prmpt.out('2.) Display Goat+StockX data on trending shoes')
+    prmpt.out('3.) Exit')
+    inp = input(">>> ")
+    
+    while not inp == '1' and not inp == '2' and not inp == '3':
+        inp = input(">>> ")
+
+    return inp
+
+
+def main():
+    opt = disp_main()
+    if opt == '1':
+        disp_reddit()
+    elif opt == '2':
+        disp_goatx()
+    else:
+        exit()
 
 if __name__ == '__main__':
     main()
--- peakdasneak/reddit_scraper.py
+++ peakdasneak/reddit_scraper.py
@@ -1,20 +1,40 @@
 from .formatter import Formatter
 from datetime import datetime
+from joblib import dump, load
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.svm import LinearSVC
 import praw
+import os.path
 
 class Reddit_Scraper:
     title = Formatter()
     date = Formatter()
     comment_total = Formatter()
+    pos = Formatter()
+    neg = Formatter()
     title.cfg(fg='w', st='b', bg='k')
     date.cfg(fg='w', st='i')
     comment_total.cfg(fg='c', st='u')
+    pos.cfg('g', st='b')
+    neg.cfg('r', st='b')
+
 
     def __init__(self, client_id, client_secret, version):
         self.reddit = praw.Reddit(client_id=client_id,
                      client_secret=client_secret,
                      user_agent=f'cli-tool: peakdasneak:v{version} (by /u/lacanlale')
-        
+    
+    # def __store(self, posts, subreddit):
+    #     if not os.path.isfile('data/comments.csv'):
+    #         f = open("data/comments.csv", "x")
+    #         f.write('subreddit,post_id,post_title,post_comment_id,post_comment,sentiment\n')
+    #         f.close()
+
+    #     with open('data/comments.csv', 'w') as data_file:
+    #         for post in posts:
+    #             for comment in posts.comments:
+    #                 data_file.write(f'{subreddit},{post.id},{post.title},{comment},{comment.body},')
+
     def scrape(self, subreddit, flair=None, limit=10, sort_by='relevance', posts_from='week'):
         """
         Scrapes specific info from stated subreddit
@@ -31,13 +51,51 @@ class Reddit_Scraper:
             Should be between 'relevance', 'top', 'new', and 'comments'. 'relevance' by default.
         posts_from : str
             Should be between 'week', 'hour', '24 hours', 'month', 'year', 'all time'. 'week' by default
+
+        Returns
+        -------
+        urls : dict
+            Url of corresponding post
         """
+        urls = {}
+        posts = []
+        clf = load('model.joblib')
+        ngram_vectorizer = load('ngram_vectorizer.joblib')
+        
+        counter = 1
         sub = self.reddit.subreddit(subreddit)
+        avg = lambda l: sum(l) / len(l)
         for post in sub.search(f'flair:"{flair}"', sort=sort_by, time_filter=posts_from):
             date = datetime.fromtimestamp(post.created_utc)
             diff = datetime.utcnow() - date
             date = f"{date.month}/{date.day}/{date.year}"
-            
-            self.title.out(post.title, ' | ')
+
+            posts.append(post)
+            urls[str(counter)] = post.url
+
+            self.title.out(f"{counter}.) {post.title}", ' | ')
             self.date.out(f"Date created: {date}, {diff.days} days ago", ' | ')
-            self.comment_total.out(f"Comment Total: {post.num_comments}")
+            self.comment_total.out(f"Comment Total: {post.num_comments}", ' | ')
+
+            sent = []
+            for comment in post.comments:
+                sent.append(clf.predict(ngram_vectorizer.transform([comment.body])))
+            
+            if len(sent) != 0:
+                avg_sent = avg(sent)
+                if avg_sent >= 0.8:
+                    self.pos.out("Extremely Positive")
+                elif avg_sent >= 0.6 and avg(sent) < 0.8:
+                    self.pos.out("Mostly Positive")
+                elif avg_sent >= 0.40 and avg_sent < 0.60:
+                    print("Neutral")
+                elif avg_sent < 0.40 and avg_sent >= 0.20:
+                    self.neg.out("Mostly Negative")
+                elif avg_sent < 0.2:
+                    self.neg.out("Extremely Negative")
+            else:
+                print("No comments to analyze sentiment")
+
+            counter += 1
+        
+        return urls
